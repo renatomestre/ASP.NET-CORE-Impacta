06ddff2b060ab90040ae05fc36830315130d625b
Renato Mestre <renato.mestre@hotmail.com>
2022-11-18 13:10:53 -0300

    Add project files.

---
+++ Capitulo02.Labs/App/WebApp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Web;
+
+namespace Lab01.App
+{
+    public static class WebApp
+    {
+        private static string comentarioArquivo
+        {
+            get
+            {
+                return HttpContext.Current.Server.MapPath("~/comentarios.txt");
+            }
+        }
+
+        public static void ComentarioIncluir(string nome, string comentario)
+        {
+            using (var writer = new StreamWriter(comentarioArquivo, true, Encoding.UTF8))
+            {
+                writer.WriteLine(
+                $"{DateTime.Now:dd/MM/yyyy} - {DateTime.Now:HH:mm:ss}");
+                writer.WriteLine($"{nome}: {comentario}\r\n");
+            }
+        }
+
+        public static string ComentariosObter()
+        {
+            string texto = string.Empty;
+            if (!File.Exists(comentarioArquivo))
+            {
+                return texto;
+            }
+            using (var reader = new StreamReader(comentarioArquivo))
+            {
+                texto = reader.ReadToEnd();
+            }
+            return texto;
+        }
+    }
+}
---
+++ Lab01/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Capitulo01.Conceitos.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
