086585c84a4091d03bac912f5c8c59c6e99e4ce1
Renato Mestre <renato.mestre@hotmail.com>
2022-11-21 16:12:18 -0300

    Products CRUD

--- Capitulo04.HelloEntityFramework/Data/NorthWindContext.cs
+++ Capitulo04.HelloEntityFramework/Data/NorthWindContext.cs
@@ -1,36 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using Capitulo04.HelloEntityFramework.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Capitulo04.HelloEntityFramework.Models;
 
-namespace Capitulo04.HelloEntityFramework.Data;
-
-public partial class NorthWindContext : DbContext
+namespace Capitulo04.HelloEntityFramework.Data
 {
-    public NorthWindContext()
+    /// <summary>
+    /// Gen code:
+    /// scaffold-dbcontext -provider Microsoft.EntityFrameworkCore.SqlServer -connection "Data Source=Localhost; Initial Catalog=NorthWind; trusted_connection=yes; Encrypt=false" -outputdir "Models" -ContextDir "Data" -DataAnnotations -table "Products" -Force
+    /// </summary>
+    public partial class NorthWindContext : DbContext
     {
-    }
-
-    public NorthWindContext(DbContextOptions<NorthWindContext> options)
-        : base(options)
-    {
-    }
-
-    public virtual DbSet<Customer> Customers { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=Localhost; Initial Catalog=NorthWind; trusted_connection=yes; Encrypt=false");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Customer>(entity =>
+        public NorthWindContext()
         {
-            entity.Property(e => e.CustomerId).IsFixedLength();
-        });
+        }
 
-        OnModelCreatingPartial(modelBuilder);
+        public NorthWindContext(DbContextOptions<NorthWindContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=Localhost; Initial Catalog=NorthWind; trusted_connection=yes; Encrypt=false");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.CustomerId).IsFixedLength();
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ReorderLevel).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitPrice).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsInStock).HasDefaultValueSql("((0))");
+
+                entity.Property(e => e.UnitsOnOrder).HasDefaultValueSql("((0))");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
     }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
