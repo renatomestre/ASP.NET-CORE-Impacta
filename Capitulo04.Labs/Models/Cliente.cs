49109aad5af88a7ddfc46a2c473a7e1055cd830c
Renato Mestre <renato.mestre@hotmail.com>
2022-11-23 14:33:30 -0300

    Demo de Code First com EF 6 (Capitulo05.Labs)

--- Capitulo04.Labs/Models/Cliente.cs
+++ Capitulo04.Labs/Models/Cliente.cs
@@ -1,4 +1,4 @@
-﻿]using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Capitulo04.Labs.Models
---
+++ Capitulo05.Labs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EF6CodeFirstDemo
+{
+    /// <summary>
+    /// https://github.com/entityframeworktutorial/EF6-Code-First-Demo
+    /// </summary>
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var student = new Student() { StudentName = "Bill" };
+
+                ctx.Students.Add(student);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Demo completed.");
+            Console.ReadLine();
+        }
+    }
+}
