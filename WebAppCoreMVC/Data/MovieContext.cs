3530caed714b57d3e6d97ac65dc0f570989da94c
Renato Mestre <renato.mestre@hotmail.com>
2022-11-25 16:11:05 -0300

    Movies

---
+++ WebAppCoreMVC/Data/MovieContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppCoreMVC.Models;
+
+namespace WebAppCoreMVC.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options) { }
+
+        public DbSet<Movie> Movies { get; set; } = null!;
+    }
+}
