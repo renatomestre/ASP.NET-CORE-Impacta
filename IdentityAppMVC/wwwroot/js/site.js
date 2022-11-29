563c78fbdf345773f0b105e17d150003ffd9bb05
Renato Mestre <renato.mestre@hotmail.com>
2022-11-29 14:54:39 -0300

    .

--- IdentityAppMVC/wwwroot/js/site.js
+++ IdentityAppMVC/wwwroot/js/site.js
@@ -3,10 +3,11 @@
 
 // Write your JavaScript code.
 $(document).ready(function () {
-    console.log('aqui');
+    console.log('Inicializando o DataTable.net...');
     $('#table-pessoas').DataTable({
         language: {
             url: '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json'
         }
     });
+    console.log('DataTable.net inicializado!');
 });
