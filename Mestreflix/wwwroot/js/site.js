8e34b5f0dc4b949f4a72600427ea8a0b08154783
Renato Mestre <renato.mestre@hotmail.com>
2022-11-29 15:10:13 -0300

    Tu-Dum button on Mestreflix

--- Mestreflix/wwwroot/js/site.js
+++ Mestreflix/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function playNetflixSound() {
+    let audio = new Audio('mp3/netflix-jingle.mp3');
+    audio.play();
+}
