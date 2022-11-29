f2c87bf47e94dfaaf11035463d522e680f50be38
Renato Mestre <renato.mestre@hotmail.com>
2022-11-29 14:48:34 -0300

    Using component https://datatables.net/

--- IdentityAppMVC/wwwroot/js/site.js
+++ IdentityAppMVC/wwwroot/js/site.js
@@ -2,3 +2,11 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    console.log('aqui');
+    $('#myTable').DataTable({
+        language: {
+            url: '//cdn.datatables.net/plug-ins/1.13.1/i18n/pt-BR.json'
+        }
+    });
+});
