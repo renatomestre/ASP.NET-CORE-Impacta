556446def261a08f6ad8ed60b65e8516e8732d60
Renato Mestre <renato.mestre@hotmail.com>
2022-11-30 16:45:57 -0300

    Exemplo de WebApi Core

---
+++ WebApi-Introducao/Controllers/AlunosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Introducao.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AlunosController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Chamada()
+        {
+            return new string[] { "Renato", "Igor", "Andressa", "João" };
+        }
+    }
+}
---
+++ WebApi-Introducao/Controllers/ReceitasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi_Introducao.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReceitasController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Index()
+        {
+            return BolosDaVovó().Union(Minhas()).OrderBy(receita => receita).ToArray();
+        }
+
+        [HttpGet(nameof(BolosDaVovó))]
+        public string[] BolosDaVovó()
+        {
+            return new string[] { "Laranja", "Fubá", "Cenoura", "Chocolate" };
+        }
+
+        [HttpGet(nameof(Minhas))]
+        public string[] Minhas()
+        {
+            return new string[] { "Pão de cebolete", "Cafeleite", "Batafrita" };
+        }
+
+        [HttpDelete("{todas}")]
+        public ActionResult Delete(bool todas)
+        {
+            if (todas)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                return NoContent();
+            }
+        }
+    }
+}
