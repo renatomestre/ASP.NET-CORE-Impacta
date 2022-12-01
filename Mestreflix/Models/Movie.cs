4024c3496ce9af9ecb35055a076aa4044c706e8f
Renato Mestre <renato.mestre@hotmail.com>
2022-12-01 15:18:56 -0300

    Etiquetas em português Movie form Partial Tratamento input moeda BRL (R$) Botão play mouse hover

--- Mestreflix/Models/Movie.cs
+++ Mestreflix/Models/Movie.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Mestreflix.Models
@@ -9,18 +10,23 @@ namespace Mestreflix.Models
         public int Id { get; set; }
 
         [Required]
+        [DisplayName("Título")]
         public string Title { get; set; }
 
         [Required]
+        [DisplayName("Gênero")]
         public string Genre { get; set; }
 
         [DataType(DataType.Date)]
+        [DisplayName("Lançamento")]
         public DateTime ReleaseDate { get; set; }
 
         [Required]
+        [DisplayName("Alugue por")]
         public decimal Price { get; set; }
 
         [Required]
+        [DisplayName("Capa (formato base64)")]
         public string Cover { get; set; }
 
         public List<Review> Reviews { get; set; } = new List<Review>();
